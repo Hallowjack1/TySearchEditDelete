ca3b61a445aaf7d94dcf6368d42bcd265e9b882a
Matthew <matthewty94@gmail.com>
2023-03-12 20:45:04 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/tysearcheditdelete/MainActivity.java
@@ -0,0 +1,34 @@
+package com.demo.tysearcheditdelete;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button CreateNewEmp, ManageRecords;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); setContentView(R.layout.activity_main);
+        CreateNewEmp = (Button) findViewById(R.id.CreateNewEmp);
+        ManageRecords = (Button) findViewById(R.id.ManageRecords);
+
+        CreateNewEmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent in = new Intent(MainActivity.this, CreateNewCustomer.class);
+                startActivity(in);
+            }
+        });
+        ManageRecords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(MainActivity.this, ManageRecords.class);
+                startActivity(in);
+            }
+        });
+    }
+}
